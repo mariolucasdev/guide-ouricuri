8c80fbb590eb19b9b64d398727bf36fc206a9727
Henrique Nascimento <henriquenascandrade@gmail.com>
2018-02-16 12:57:35 -0300

    finalization of the links of the page about

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,10 @@ export default class About extends Component {
     return (
       <div className="container background">
         <h1 className="title-about">Ourikas Reactjs</h1>
-        <p className="text-about">Website baseado no projeto Ourikas cujo o autor tanto dos dados usados aqui quanto da ideia de fazer um guia da cidade de Ouricuri é: <a href="https://github.com/mauriciomelo">Mauricio Melo</a></p>
-        <p className="text-about">Reposotio oficial do projeto: <a href="">Ourikas</a></p>
+        <p className="text-about">Website based on the Ourikas project whose author both the data used here and the idea of ​​making a guide of the city of Ouricuri is: <a href="https://github.com/mauriciomelo">Mauricio Melo</a></p>
+        <p className="text-about">Official project repository: <a href="https://github.com/Ourikas/ourikas">Ourikas</a></p>
         <br />
-        <p className="text-about">Reposotio desse projeto: <a href="">Guide Ouricuri</a></p>
-        
+        <p className="text-about">Repository for this project: <a href="https://github.com/henriquenasc/guide-ouricuri">Guide Ouricuri</a></p>
       </div>
     );
   }
