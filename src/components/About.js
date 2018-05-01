aace1ba45ab3856b4004179e2ecd23809f749f36
Mário Lucas <maryosk8bkt@gmail.com>
2018-04-30 22:24:22 -0300

    refactory components

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class About extends Component {
-  render() {
-    return (
-      <div className="container background">
-        <h1 className="title-about">Ourikas Reactjs</h1>
-        <p className="text-about">Website based on the Ourikas project whose author both the data used here and the idea of ​​making a guide of the city of Ouricuri is: <a href="https://github.com/mauriciomelo">Mauricio Melo</a></p>
-        <p className="text-about">Official project repository: <a href="https://github.com/Ourikas/ourikas">Ourikas</a></p>
-        <br />
-        <p className="text-about">Repository for this project: <a href="https://github.com/henriquenasc/guide-ouricuri">Guide Ouricuri</a></p>
-      </div>
-    );
-  }
-}
+export default () => {
+  return (
+    <div className="container background">
+      <h1 className="title-about">Ourikas Reactjs</h1>
+      <p className="text-about">
+        Website based on the Ourikas project whose author both the data used
+        here and the idea of ​​making a guide of the city of Ouricuri is:{' '}
+        <a href="https://github.com/mauriciomelo">Mauricio Melo</a>
+      </p>
+      <p className="text-about">
+        Official project repository:{' '}
+        <a href="https://github.com/Ourikas/ourikas">Ourikas</a>
+      </p>
+      <br />
+      <p className="text-about">
+        Repository for this project:{' '}
+        <a href="https://github.com/henriquenasc/guide-ouricuri">
+          Guide Ouricuri
+        </a>
+      </p>
+    </div>
+  );
+};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react'
-import {
-  Link,
-} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { companies: [], text: '' }
-    this.search = this.search.bind(this)
+    this.state = { companies: [], text: '' };
+    this.search = this.search.bind(this);
 
-    this.companiesSearch = []
+    this.companiesSearch = [];
   }
 
   componentDidMount() {
     fetch('http://ourikas.github.io/companies.json')
       .then(res => res.json())
-      .then(dados => this.setState({ companies: dados }))
+      .then(dados => this.setState({ companies: dados }));
   }
 
   search(text) {
-    this.setState({ text: text.target.value })
-    if(text.target.value == '') {
-      this.companiesSearch = []
-    }else {
+    this.setState({ text: text.target.value });
+    if (text.target.value === '') {
+      this.companiesSearch = [];
+    } else {
       this.companiesSearch = this.state.companies.filter(item => {
         const itemName = item.name.toUpperCase();
         const textInput = this.state.text.toUpperCase();
         return itemName.indexOf(textInput) > -1;
       });
     }
-
-    console.log(this.state.text)
   }
 
   render() {
@@ -42,19 +38,21 @@ export default class Home extends Component {
           type="text"
           placeholder="search un company..."
           className="form-control"
-          onChange={(text) => this.search(text)}
+          onChange={text => this.search(text)}
         />
 
-        {this.companiesSearch.map(item => <Link to={`/details/${item.name}`} key={item.id}>
-          <div class="card content">
-            {/* <img class="card-img-top" src="..." alt="Card image cap" /> */}
+        {this.companiesSearch.map(item => (
+          <Link to={`/details/${item.name}`} key={item.id}>
+            <div class="card content">
+              {/* <img class="card-img-top" src="..." alt="Card image cap" /> */}
               <div class="card-block">
                 <h4 class="card-title">{item.name}</h4>
                 <p class="card-text">{item.description}</p>
               </div>
-          </div>
-        </Link>)}
+            </div>
+          </Link>
+        ))}
       </div>
-        )
-        }
-        }
+    );
+  }
+}
