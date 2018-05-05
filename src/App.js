223a64075df1c4169fdc7299571b6bbd19a9dc0c
Henrique Nascimento <henriquenascandrade@gmail.com>
2018-05-05 13:47:21 -0300

    add bulma container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
+import "bulma/css/bulma.css";
+import "./style.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Search />
+      <div className="section">
+        <div className="container is-fluid">
+          <div>
+            <h1 className="title is-1 logo-title">Agenda Ouricuri</h1>
+            <Search />
+          </div>
+        </div>
       </div>
     );
   }
