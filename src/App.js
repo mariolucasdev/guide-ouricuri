230f6942acdeefad48df9f7cb63d1da689374984
Mário Lucas <maryosk8bkt@gmail.com>
2018-05-06 14:13:44 -0300

    refactory components struct

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { Component } from "react";
-import Search from "./components/Search";
-import "bulma/css/bulma.css";
-import "./style.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import Search from './components/Search';
+import 'bulma/css/bulma.css';
+import './style.css';
 
 class App extends Component {
+  state = {
+    companies: []
+  };
+
+  async componentDidMount() {
+    const response = await axios.get('http://ourikas.github.io/companies.json');
+    this.setState({ companies: response.data });
+  }
+
   render() {
     return (
       <div className="section">
         <div className="container is-fluid">
           <div>
             <h1 className="title is-1 logo-title">Agenda Ouricuri</h1>
-            <Search />
+            <Search companies={this.state.companies} />
           </div>
         </div>
       </div>
